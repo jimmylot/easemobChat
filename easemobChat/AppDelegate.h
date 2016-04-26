4e065163c1b816398074fc00f112852dee3774d5
FanLang <fanlang@fanlangdemacbook-air.local>
2016-04-26 17:06:42 +0800

    Initial Commit

---
+++ easemobChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  easemobChat
+//
+//  Created by FanLang on 16/4/26.
+//  Copyright © 2016年 FanLang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
