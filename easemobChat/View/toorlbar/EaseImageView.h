2729c9c784d503ea5ba1cd4cda0ef034049f28a1
FanLang <fanlang@fanlangdemacbook-air.local>
2016-05-11 09:53:39 +0800

    分装 字体大小 颜色

---
+++ easemobChat/View/toorlbar/EaseImageView.h
@@ -0,0 +1,32 @@
+//
+//  SessionCell.h
+//  easemobChat
+//
+//  Created by FanLang on 16/4/28.
+//  Copyright © 2016年 FanLang. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface EaseImageView : UIView
+
+@property (strong, nonatomic) UIImageView *imageView;
+
+@property (strong, nonatomic) UIImage *image;
+
+@property (nonatomic) NSInteger badge;
+
+@property (nonatomic) BOOL showBadge;
+
+@property (nonatomic) CGFloat imageCornerRadius UI_APPEARANCE_SELECTOR;
+
+@property (nonatomic) CGFloat badgeSize UI_APPEARANCE_SELECTOR;
+
+@property (nonatomic) UIFont *badgeFont UI_APPEARANCE_SELECTOR;
+
+@property (nonatomic) UIColor *badgeTextColor UI_APPEARANCE_SELECTOR;
+
+@property (nonatomic) UIColor *badgeBackgroudColor UI_APPEARANCE_SELECTOR;
+
+@end
